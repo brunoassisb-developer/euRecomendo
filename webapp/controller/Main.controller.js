40cffbafeb0430c8278bd9dc100ccdfcd5406959
Barbosa Bruno <bruno.barbosa@linkupconsultoria.com.br>
2018-01-22 17:39:31 +0000

    Autenticação Usuário

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -111,6 +111,125 @@ sap.ui.define([
 			onFilterNo: function(oEvent){
 				this._oDialogFilterTiles.close();
 			},
+
+			/**
+			 * @public
+			 */				
+			onLogon: function(oEvent){
+				//Abre Tela de Logon
+	            if (! this._oDialogLogon) {
+	               var oView = this.getView();
+	               this._oDialogLogon = sap.ui.xmlfragment(oView.getId(),"com.sap.euRecomendo.view.fragments.logon", this);
+	            }
+
+	            this.getView().addDependent(this._oDialogLogon);
+	            
+	            // toggle compact style
+	            jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._oDialogLogon);
+	            this._oDialogLogon.open();	
+			},
+			
+			/**
+			 * @public
+			 */				
+			onLogonConfirm: function(oEvent){
+				var sUser = this.byId("inputUser").getValue(),
+					sPasswrd = this.byId("inputPassword").getValue();
+					
+				if(!sUser || !sPasswrd){
+					MessageBox.error(this.getResourceBundle().getText("msgErrorFillAllFields"), 
+					                  { styleClass: this.getOwnerComponent().getContentDensityClass() }
+					         );
+					return;
+				}
+				
+				//Set User information as Global Model
+				var oUser = {
+								user: sUser,
+								img: "img/user.jpg"
+							};
+				
+				var oGlobalModel = new sap.ui.model.json.JSONModel(oUser);
+  					sap.ui.getCore().setModel(oGlobalModel, "globalData");
+				
+				//Set Foto User Main View
+				this.byId("logonButton").setIcon(oUser.img);
+				this.onLogonClose();
+			},
+
+			/**
+			 * @public
+			 */				
+			onLogonClose: function(oEvent){
+				this._oDialogLogon.destroy();
+				this._oDialogLogon = null;
+			},
+
+			/**
+			 * @public
+			 */				
+			onNewUser: function(oEvent){
+				
+				this.onLogonClose();
+				
+					//Abre Tela de Novo Usuário
+	            if (! this._oDialogNewUser) {
+	               var oView = this.getView();
+	               this._oDialogNewUser = sap.ui.xmlfragment(oView.getId(),"com.sap.euRecomendo.view.fragments.newUser", this);
+	            }
+
+	            this.getView().addDependent(this._oDialogNewUser);
+	            
+	            // toggle compact style
+	            jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._oDialogNewUser);
+	            this._oDialogNewUser.open();	
+			},
+
+			/**
+			 * @public
+			 */				
+			onNewUserClose: function(oEvent){
+				this._oDialogNewUser.destroy();
+				this._oDialogNewUser = null;
+			},
+
+			/**
+			 * @public
+			 */				
+			onNewUserConfirm: function(oEvent){
+				
+				var sUser = this.byId("inputNewUser").getValue(),
+					sPasswrd = this.byId("inputNewPassword").getValue(),
+					sConfPasswrd = this.byId("inputConfirmNewPassword").getValue();
+					
+				if(!sUser || !sPasswrd || !sConfPasswrd){
+					MessageBox.error(this.getResourceBundle().getText("msgErrorFillAllFields"), 
+					                  { styleClass: this.getOwnerComponent().getContentDensityClass() }
+					         );
+					return;
+				}
+				
+				if(sPasswrd !== sConfPasswrd){
+					MessageBox.error(this.getResourceBundle().getText("msgErrorPasswdConfPwsd"), 
+					                  { styleClass: this.getOwnerComponent().getContentDensityClass() }
+					         );
+					return;
+				}
+				
+				//Set User information as Global Model
+				var oUser = {
+								user: sUser,
+								img: "img/user.jpg"
+							};
+				
+				var oGlobalModel = new sap.ui.model.json.JSONModel(oUser);
+  					sap.ui.getCore().setModel(oGlobalModel, "globalData");
+				
+				//Set Foto User Main View
+				this.byId("logonButton").setIcon(oUser.img);
+				this.onNewUserClose();	
+			},
+			
 			/* =========================================================== */
 			/* internal methods                                            */
 			/* =========================================================== */
